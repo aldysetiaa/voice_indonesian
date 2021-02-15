84751df1d795d7e41169ec7443a1782de47639e3
Aldy Setiawan <70496872+aldysetiaa@users.noreply.github.com>
2021-02-15 14:04:15 +0700

    Add files via upload

---
+++ vcs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <title>Antrian Suara</title>
+    <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="ajax_jquery.js"></script>
+  </head>
+   <body onload="speak('Antrian nomor D   26 dengan nomor perkara  Harap ke Meja Pengambilan Salinan Putusan dan Aktecerai')" >
+    <select hidden=""> 
+    </select>  
+    <audio id="tingtong"  src="1.mp3"></audio>
+    <script src="script.js"></script>
+    </body>
+</html>
